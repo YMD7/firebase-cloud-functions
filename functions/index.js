578cefc706feb02a6f7ac66183a6fc0aa932380f
Kyo Yamada <hysteric.madam@gmail.com>
2019-06-30 15:39:45 +0900

    add makeUppercase()

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,19 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// addMessage()
 exports.addMessage = functions.https.onRequest(async (req, res) => {
   const original = req.query.text;
   const snapshot = await admin.database().ref('/messages').push({original: original});
   res.redirect(303, snapshot.ref.toString());
 });
+
+// makeUppercase()
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+  .onCreate((snapshot, context) => {
+    const original = snapshot.val();
+    console.log('Uppercasing', context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    return snapshot.ref.parent.child('uppercase').set(uppercase);
+  });
 
